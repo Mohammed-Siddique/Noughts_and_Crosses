7f396e76845d246ed236f5a9f27b3c79008f4a8b
Mohammed Siddique <khanshahid9066@gmail.com>
2025-06-19 07:38:58 +0530

    Noughts & Crosses
    DOM manipulation using javascript

--- app.js
+++ app.js
@@ -1,73 +1,21 @@
-let boxBtn = document.querySelectorAll('#box');
-let resetBtn = document.querySelector('.reset');
-let msgContainer = document.querySelector('.msg-container');
-let secondContainer = document.querySelector('.second-container');
-let mainHeading = document.querySelector('main-heading');
-let turn0 = true;
+let boxes = document.querySelectorAll('.box');
+let resetBtn = document.querySelector('#reset-btn');
 
-const winPattern = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 5],
-    [1, 4, 7],
-    [2, 4, 6],
-    [2, 5, 8],
-    [3, 4, 5],
-    [6, 7, 8]
-]
+let turn = true; // player X or player O
 
-boxBtn.forEach((box) => {
+const winPatterns = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8]
+];
+
+boxes.forEach((box) => {
     box.addEventListener('click', () => {
-        if (turn0) {
-            box.textContent = "O";
-            turn0 = false
-        } else {
-            box.textContent = "X"
-            turn0 = true
-        }
-        box.disabled = true;
-        
-        gameWinner();
-        
-    });
-});
-
-
-const gameWinner = ()=> {
-    for (let pattern of winPattern) { 
-        let val1 =  boxBtn[pattern[0]].textContent;
-        let val2 =  boxBtn[pattern[1]].textContent; 
-        let val3 =  boxBtn[pattern[2]].textContent
-        
-        if (val1 != "" && val2 != "" && val3 != "") {
-            if (val1 === val2 && val2 === val3) {
-                console.log('Winner!', val1)
-                msgContainer.removeAttribute('class', 'block-container');
-                secondContainer.setAttribute('class', 'block-second-container');  
-            }
-        };   
-    }   
-};
-
-const resetButton = ()=> {
-    resetBtn.addEventListener('click', () => {
-        boxBtn.forEach((box) => {
-            box.textContent = "";
-        })
-    })  
-};
-
-resetButton();
-
-
-
-
-
-
-
-
-
-
-
-
-
+        console.log('box was clicked')
+    })
+})
