7bb5748b486d233b607eff90ba78d52de9e7d90a
Mohammed Siddique <khanshahid9066@gmail.com>
2025-06-20 23:43:51 +0530

    Noughts & Crosses
    DOM manipulation using javascript

--- app.js
+++ app.js
@@ -33,5 +33,9 @@ boxes.forEach((box) => {
 const checkWinner = () => {
     for(let pattern of winPatterns) {
         console.log(pattern[0], pattern[1], pattern[2])
+        console.log(
+            boxes[pattern[0]].innerText,
+            boxes[pattern[1]].innerText, 
+            boxes[pattern[2]].innerText)
     }
 }
